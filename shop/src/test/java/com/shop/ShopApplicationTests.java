d80fd7b6fa7b50867b807b7fa54d059d525f01ee
tseop <77163842+tseop@users.noreply.github.com>
2022-06-14 15:52:39 +0900

    Chapter03 update
    Chapter03 update

--- shop/src/test/java/com/shop/ShopApplicationTests.java
+++ shop/src/test/java/com/shop/ShopApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDemoApplicationTests {
+class ShopApplicationTests {
 
 	@Test
 	void contextLoads() {
