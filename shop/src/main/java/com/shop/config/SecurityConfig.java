a503a47911cb746756092ff947225809ffe4f100
tseop <77163842+tseop@users.noreply.github.com>
2022-06-17 15:45:03 +0900

    Chapter04 close-out
    [챕터 4] 스프링 시큐리티 종료
    - 회원가입 페이지 구현
    - 회원가입 컨트롤러 구현
    - 회원가입 엔티티 유효성 검증 추가
    - 로그인 기능 페이지 및 기능 구현
    - 로그아웃 기능 구현
    - 페이지별 권한 설정 구현
    - 권한별 페이지 접근 테스트 완료

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -1,20 +1,58 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter { // deprecated 되었지만 예제가 진행되는 동안은 사용한다.
 
+    @Autowired
+    private MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login") // 로그인 페이지 URL 설정
+                .defaultSuccessUrl("/")  // 로그인 성공시 이동할 URL 설정
+                .usernameParameter("email") // 로그인 시 사용할 파라미터 이름 지정
+                .failureUrl("/members/login/error") // 로그인 실패시 이동할 페이지
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 URL 설정
+                .logoutSuccessUrl("/");
 
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**")
+                .permitAll()
+                .mvcMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
